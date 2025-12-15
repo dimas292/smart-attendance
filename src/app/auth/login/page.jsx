846884a6c6c9e3bacca77e23e7d7dec76d5663f5
Dimaslab <dimasixc22@gmail.com>
2025-12-15 18:38:50 +0700

    feat: add form validation with Formik and Yup to login page

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -9,12 +9,22 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import * as Yup from "yup"
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
 
 export default function LoginPage() {
   const isDark = useStore((state) => state.isDark);
   const toggleTheme = useStore((state) => state.toggleTheme);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const validationLoginScemema = Yup.object({
+    username: Yup.string().min(6, "Username must be at least 6 characters").required("Username is required").max(20, "Username must be at most 20 characters"),
+    password: Yup.string().min(8, "Password must be at least 8 characters").required("Password is required").matches(/[a-zA-Z]/, "Password must contain at least one letter").matches(/[0-9]/, "Password must contain at least one number"),
+  })
+
+
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
@@ -26,12 +36,6 @@ export default function LoginPage() {
       toggleTheme();
     }
   }, [isDark, toggleTheme]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    // TODO: hubungkan ke backend auth
-    console.log({ username, password, remember });
-  }
 
   return (
     <div className="flex items-center justify-center min-h-screen">
@@ -54,50 +58,64 @@ export default function LoginPage() {
               Masuk menggunakan akun Anda untuk melanjutkan.
             </CardDescription>
           </CardHeader>
+          <Formik
+            initialValues={{ username: "", password: "" }}
+            validationSchema={validationLoginScemema}
+            onSubmit={(values) => {
+              console.log("Form data", values);
+            }}>
+            {({ isSubmitting }) => (
+      
+              <Form className="mt-4 flex flex-col gap-4">
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="username" className="text-sm font-medium">
+                    Username
+                  </label>
+                  <Field
+                    id="username"
+                    name="username"
+                    type="text"
+                    className="w-full max-w-md border rounded-md px-3 py-2 focus:ring-2 focus:ring-primary/30"
+                    placeholder="Masukkan username"
+                    aria-label="username"
+                  />
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="text-sm text-red-600"
+                  />
+                </div>
 
-          <form onSubmit={handleSubmit} className="mt-4 flex flex-col gap-4">
-            <div className="flex flex-col gap-1">
-              <label htmlFor="username" className="text-sm font-medium">
-                Username
-              </label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                required
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="w-full max-w-md border rounded-md px-3 py-2 focus:ring-2 focus:ring-primary/30"
-                placeholder="Masukkan username"
-                aria-label="username"
-              />
-            </div>
-
-            <div className="flex flex-col gap-1">
-              <label htmlFor="password" className="text-sm font-medium">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full max-w-md border rounded-md px-3 py-2 focus:ring-2 focus:ring-primary/30"
-                placeholder="Masukkan password"
-                aria-label="password"
-              />
-            </div>
-            <div className="pt-2">
-              <Button
-                type="submit"
-                className="w-full max-w-md bg-gradient-to-r from-primary to-emerald-500 text-xl font-bold"
-              >
-                Login
-              </Button>
-            </div>
-          </form>
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="password" className="text-sm font-medium">
+                    Password
+                  </label>
+                  <Field
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="w-full max-w-md border rounded-md px-3 py-2 focus:ring-2 focus:ring-primary/30"
+                    placeholder="Masukkan password"
+                    aria-label="password"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-sm text-red-600"
+                  />
+                </div>
+                <div className="pt-2">
+                  <Button
+                    type="submit"
+                    className="w-full max-w-md bg-gradient-to-r from-primary to-emerald-500 text-xl font-bold"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? "Logging in..." : "Login"}
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </CardContent>
       </Card>
     </div>
